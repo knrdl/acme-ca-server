e1665c0016700ed93f469b09cbaa105e81bbd22b
Bas van Driel <contact@basvandriel.nl>
2024-11-11 08:35:54 +0100

    Finalize new account

--- tests/test_order_gen.py
+++ tests/test_order_gen.py
@@ -8,10 +8,6 @@ import base64
 from jwcrypto import jwk, jws
 
 
-def base64url_encode(data: bytes) -> str:
-    return base64.urlsafe_b64encode(data).decode("utf-8").rstrip("=")
-
-
 def create_nonce(testclient: TestClient):
     nonce_response = testclient.head("/acme/new-nonce")
     nonce_headers = nonce_response.headers
@@ -22,10 +18,7 @@ def create_nonce(testclient: TestClient):
     return nonce
 
 
-def test_create_new_account(fastapi_testclient: TestClient):
-    # Before order generation, create a nonce
-    nonce = create_nonce(fastapi_testclient)
-
+def create_account_response(testclient: TestClient, nonce: str):
     # TODO replace this with self created public key
     jwk_key = jwk.JWK.generate(kty="EC", crv="P-256")
     jwk_export = jwk_key.export(private_key=False, as_dict=True)
@@ -39,7 +32,11 @@ def test_create_new_account(fastapi_testclient: TestClient):
         "jwk": jwk_export,
     }
 
-    payload_data = {"termsOfServiceAgreed": True}
+    payload_data = {
+        "termsOfServiceAgreed": True,
+        "onlyReturnExisting": False,
+        "contact": ["mailto:contact@basvandriel.nl"],
+    }
     payload_encoded = json_encode(payload_data).encode("utf-8")
 
     jws_object = jws.JWS(payload_encoded)
@@ -47,11 +44,21 @@ def test_create_new_account(fastapi_testclient: TestClient):
 
     jws_serialized = json.loads(jws_object.serialize(compact=False))
 
-    response = fastapi_testclient.post(
+    response = testclient.post(
         "/acme/new-account",
         headers={"Content-Type": "application/jose+json"},
         json=jws_serialized,
     )
+    return response
+
+
+def test_create_new_account(fastapi_testclient: TestClient):
+    # Before order generation, create a nonce
+    nonce = create_nonce(fastapi_testclient)
+    response = create_account_response(fastapi_testclient, nonce)
+
+    # TODO retrieve account id
+    assert response.is_success
 
 
 def test_acme_order(fastapi_testclient: TestClient):
